2f57c6a3ab6f125a92b220a8118a325b62f08fe6
daryll <dosis@csumb.edu>
2018-06-04 14:28:40 -0700

    initial commit

---
+++ FirstJava.java
@@ -0,0 +1,11 @@
+/*
+Author: Daryll Osis
+Date: June 04, 2018
+Description: first java program, just a simple print line
+*/
+
+public class FirstJava {
+	public static void main(String[] args) {
+		System.out.println("My name is Daryll Osis \nI am 27 years old \nMy hometown is Colorado Springs, CO");
+	}
+}
